43b5e286a8ae099ff26a47e9b702afddea787f45
Christopher Jeffrey <chjjeffrey@gmail.com>
2015-02-07 00:06:14 -0800

    Revert "use ptsname_r to be thread safe. see #96."
    This reverts commit 23d7e49bac3e16863ab857e65e3a19384a18bd5a.

--- src/unix/pty.cc
+++ src/unix/pty.cc
@@ -454,8 +454,7 @@ pty_openpty(int *amaster, int *aslave, char *name,
             const struct termios *termp,
             const struct winsize *winp) {
 #if defined(__sun)
-  int slave_ret;
-  char slave_name[40];
+  char *slave_name;
   int slave;
   int master = open("/dev/ptmx", O_RDWR | O_NOCTTY);
   if (master == -1) return -1;
@@ -464,8 +463,8 @@ pty_openpty(int *amaster, int *aslave, char *name,
   if (grantpt(master) == -1) goto err;
   if (unlockpt(master) == -1) goto err;
 
-  slave_ret = ptsname_r(master, slave_name, sizeof(slave_name));
-  if (slave_ret != 0) goto err;
+  slave_name = ptsname(master);
+  if (slave_name == NULL) goto err;
   if (name) strcpy(name, slave_name);
 
   slave = open(slave_name, O_RDWR | O_NOCTTY);
