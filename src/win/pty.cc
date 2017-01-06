74cd392aace8a7e511331f37547d815bef88133c
Daniel Imms <tyriar@tyriar.com>
2017-01-06 14:49:32 -0800

    Re-enable env

--- src/win/pty.cc
+++ src/win/pty.cc
@@ -249,23 +249,23 @@ static NAN_METHOD(PtyStartProcess) {
   // create environment block
   wchar_t *env = NULL;
   const Handle<Array> envValues = Handle<Array>::Cast(info[2]);
-  // if(!envValues.IsEmpty()) {
+  if(!envValues.IsEmpty()) {
 
-  //   std::wstringstream envBlock;
+    std::wstringstream envBlock;
 
-  //   for(uint32_t i = 0; i < envValues->Length(); i++) {
-  //     std::wstring envValue(to_wstring(String::Utf8Value(envValues->Get(i)->ToString())));
-  //     envBlock << envValue << L' ';
-  //   }
+    for(uint32_t i = 0; i < envValues->Length(); i++) {
+      std::wstring envValue(to_wstring(String::Utf8Value(envValues->Get(i)->ToString())));
+      envBlock << envValue << L' ';
+    }
 
-  //   std::wstring output = envBlock.str();
+    std::wstring output = envBlock.str();
 
-  //   size_t count = output.size();
-  //   env = new wchar_t[count + 2];
-  //   wcsncpy(env, output.c_str(), count);
+    size_t count = output.size();
+    env = new wchar_t[count + 2];
+    wcsncpy(env, output.c_str(), count);
 
-  //   wcscat(env, L"\0");
-  // }
+    wcscat(env, L"\0");
+  }
 
   // use environment 'Path' variable to determine location of
   // the relative path that we have recieved (e.g cmd.exe)
@@ -339,7 +339,7 @@ open:
   //  assert(conin != INVALID_HANDLE_VALUE);
   //  assert(conout != INVALID_HANDLE_VALUE);
 
-  winpty_spawn_config_t* config = winpty_spawn_config_new(WINPTY_SPAWN_FLAG_AUTO_SHUTDOWN, shellpath.c_str(), cmdline, cwd, nullptr/*env*/, nullptr);
+  winpty_spawn_config_t* config = winpty_spawn_config_new(WINPTY_SPAWN_FLAG_AUTO_SHUTDOWN, shellpath.c_str(), cmdline, cwd, env, nullptr);
   HANDLE handle = nullptr;
   BOOL spawnSuccess = winpty_spawn(pc, config, &handle, nullptr, nullptr, nullptr);
   winpty_spawn_config_free(config);
