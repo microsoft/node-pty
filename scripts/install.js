98a32bdae040d20a69e3690c0fed15d87cab2f8d
Daniel Imms <daimms@microsoft.com>
2017-01-15 01:39:43 -0800

    Use node-gyp.cmd on windows

--- scripts/install.js
+++ scripts/install.js
@@ -1,10 +1,10 @@
-
 'use strict'
 
+const os = require('os');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-spawn('node-gyp', ['rebuild'], {
+spawn(os.platform() === 'win32' ? 'node-gyp.cmd' : 'node-gyp', ['rebuild'], {
   cwd: path.join(__dirname, '..'),
   stdio: 'inherit'
 });
