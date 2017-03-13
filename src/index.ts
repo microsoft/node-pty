e6992c98ffbf33a51c477a17a76fae6c7b7488fb
Daniel Imms <tyriar@tyriar.com>
2017-03-12 19:57:32 -0700

    Fix typo

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ if (os.platform() === 'win32') {
  * @param file The file to launch.
  * @param args The file's arguments as argv (string[]) or in a pre-escaped
  * CommandLine format (string). Note that the CommandLine option is only
- * available on Windows and is expected to be escaped properly as per.
+ * available on Windows and is expected to be escaped properly.
  * @param options The options of the terminal.
  * @see CommandLineToArgvW https://msdn.microsoft.com/en-us/library/windows/desktop/bb776391(v=vs.85).aspx
  * @see Parsing C++ Comamnd-Line Arguments https://msdn.microsoft.com/en-us/library/17w5ykft.aspx
