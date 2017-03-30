dc908175ee0afcdd2660a95a0451f962e77a8537
Daniel Imms <daimms@microsoft.com>
2017-03-29 21:39:28 -0700

    Add test case for mac pts name

--- test/unixTerminal.test.js
+++ test/unixTerminal.test.js
@@ -7,8 +7,16 @@ describe("UnixTerminal", function() {
   describe("Constructor", function() {
     it("should set a valid pts name", function() {
       const term = new UnixTerminal('cmd.exe', [], {});
-      // Should match form from https://linux.die.net/man/4/pts
-      assert.ok(/^\/dev\/pts\/\d+$/.test(term.pty));
+
+      if (process.platform === 'linux') {
+        // https://linux.die.net/man/4/pts
+        assert.ok(/^\/dev\/pts\/\d+$/.test(term.pty));
+      }
+
+      if (process.platform === 'darwin') {
+        // https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man4/pty.4.html
+        assert.ok(/^\/dev\/tty[p-sP-S][a-z0-9]$/.test(term.pty));
+      }
     });
   });
 });
