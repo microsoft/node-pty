f81239d2497017400b9de91fdc97ed7f94f86650
Daniel Imms <daimms@microsoft.com>
2017-06-12 14:19:08 -0700

    Ensure file is escaped correct on Windows with commandline args
    Fixes #91

--- test/windowsTerminal.test.js
+++ test/windowsTerminal.test.js
@@ -1,5 +1,6 @@
 if (process.platform !== 'win32') return
 
+var fs = require('fs');
 var assert = require("assert");
 var WindowsTerminal = require('../lib/windowsTerminal').WindowsTerminal;
 
@@ -10,6 +11,25 @@ describe("WindowsTerminal", function() {
       term.kill();
       // Add done call to deferred function queue to ensure the kill call has completed
       term._defer(done);
+    });
+  });
+
+  describe("Args as CommandLine", function(done) {
+    it("should not fail running a shell containing a space in the path", function(done) {
+      const gitBashDefaultPath = "C:\\Program Files\\Git\\bin\\bash.exe";
+      if (!fs.existsSync(gitBashDefaultPath)) {
+        //Skip test if git bash isn't installed
+        return;
+      }
+      const term = new WindowsTerminal(gitBashDefaultPath, '-c "echo helloworld"', {});
+      let result = '';
+      term.on('data', function (data) {
+        result += data;
+      });
+      term.on('exit', function() {
+        assert.ok(result.indexOf('helloworld') >= 0);
+        done();
+      });
     });
   });
 });
