ab35537a51e5b5589482feea214e88181829f285
Daniel Imms <daimms@microsoft.com>
2017-07-29 15:01:05 -0700

    Move argsToCommandLine tests to TS

--- src/windowsTerminal.test.ts
+++ src/windowsTerminal.test.ts
@@ -1,53 +1,53 @@
-if (process.platform !== 'win32') return
+import * as fs from 'fs';
+import * as assert from 'assert';
+import { WindowsTerminal } from './windowsTerminal';
 
-var fs = require('fs');
-var assert = require("assert");
-var WindowsTerminal = require('../lib/windowsTerminal').WindowsTerminal;
+if (process.platform === 'win32') {
+  describe('WindowsTerminal', () => {
+    describe('kill', () => {
+      it('should not crash parent process', (done) => {
+        const term = new WindowsTerminal('cmd.exe', [], {});
+        term.kill();
+        // Add done call to deferred function queue to ensure the kill call has completed
+        (<any>term)._defer(done);
+      });
+    });
 
-describe("WindowsTerminal", function() {
-  describe("kill", function() {
-    it("should not crash parent process", function(done) {
-      const term = new WindowsTerminal('cmd.exe', [], {});
-      term.kill();
-      // Add done call to deferred function queue to ensure the kill call has completed
-      term._defer(done);
+    describe('Args as CommandLine', () => {
+      it('should not fail running a shell containing a space in the path', (done) => {
+        const gitBashDefaultPath = 'C:\\Program Files\\Git\\bin\\bash.exe';
+        if (!fs.existsSync(gitBashDefaultPath)) {
+          // Skip test if git bash isn't installed
+          return;
+        }
+        const term = new WindowsTerminal(gitBashDefaultPath, '-c "echo helloworld"', {});
+        let result = '';
+        term.on('data', (data) => {
+          result += data;
+        });
+        term.on('exit', () => {
+          assert.ok(result.indexOf('helloworld') >= 0);
+          done();
+        });
+      });
+    });
+
+    describe('On close', () => {
+      it('should return process zero exit codes', (done) => {
+        const term = new WindowsTerminal('cmd.exe', '/C exit');
+        term.on('exit', (code) => {
+          assert.equal(code, 0);
+          done();
+        });
+      });
+
+      it('should return process non-zero exit codes', function(done) {
+        const term = new WindowsTerminal('cmd.exe', '/C exit 2');
+        term.on('exit', function(code) {
+          assert.equal(code, 2);
+          done();
+        });
+      });
     });
   });
-
-  describe("Args as CommandLine", function(done) {
-    it("should not fail running a shell containing a space in the path", function(done) {
-      const gitBashDefaultPath = "C:\\Program Files\\Git\\bin\\bash.exe";
-      if (!fs.existsSync(gitBashDefaultPath)) {
-        //Skip test if git bash isn't installed
-        return;
-      }
-      const term = new WindowsTerminal(gitBashDefaultPath, '-c "echo helloworld"', {});
-      let result = '';
-      term.on('data', function (data) {
-        result += data;
-      });
-      term.on('exit', function() {
-        assert.ok(result.indexOf('helloworld') >= 0);
-        done();
-      });
-    });
-  });
-
-  describe("On close", function(done) {
-    it("should return process zero exit codes", function(done) {
-      const term = new WindowsTerminal('cmd.exe', '/C exit');
-      term.on('exit', function(code) {
-        assert.equal(code, 0);
-        done();
-      });
-    });
-
-    it("should return process non-zero exit codes", function(done) {
-      const term = new WindowsTerminal('cmd.exe', '/C exit 2');
-      term.on('exit', function(code) {
-        assert.equal(code, 2);
-        done();
-      });
-    });
-  });
-});
+}
