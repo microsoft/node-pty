6428c62c4da19f066ee7230602a1ba35d89d0dd5
Daniel Imms <daimms@microsoft.com>
2017-10-03 11:33:15 -0700

    Support Electron demo on Windows

---
+++ examples/electron/npm_install.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+set npm_config_disturl="https://atom.io/download/electron"
+set npm_config_target=1.6.2
+set npm_config_runtime="electron"
+set npm_config_cache=~\.npm-electron
+npm i
+endlocal
