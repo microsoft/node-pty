ef7ff50c7f824c09554601857333757f081c2044
Andres Mejia <amejia004@gmail.com>
2018-04-03 20:33:16 -0400

    Use a dedicated script for building prebuilt binaries. Script will mainly be used to ensure prebuilt binaries are only built for releases.

---
+++ scripts/prebuild.js
@@ -0,0 +1,52 @@
+const { spawnSync } = require('child_process');
+const path = require('path');
+
+const pkg = require(path.resolve('package.json'));
+
+const regex = new RegExp('^\\d+\.\\d+\.\\d+$');
+
+function spawnPrebuild(args) {
+  console.log(process.arch);
+  console.log(args);
+  let command = 'prebuild';
+  if (process.platform === 'win32') {
+    command = 'prebuild.cmd';
+  }
+  let p = spawnSync(
+    command,
+    args,
+    {
+      stdio: 'inherit'
+    }
+  );
+  if (p.error) {
+    throw p.error;
+  }
+}
+
+function main() {
+  let baseArgs = [
+    '--all',
+    '--strip',
+    '--verbose'
+  ];
+  if (pkg.version.match(regex) && process.env.PREBUILD_UPLOAD_TOKEN) {
+    baseArgs = baseArgs.concat([
+      '--upload',
+      process.env.PREBUILD_UPLOAD_TOKEN
+    ]);
+  }
+  let args = baseArgs;
+  spawnPrebuild(args);
+  if (process.arch === 'x64' && (process.platform === 'linux' || process.platform === 'win32')) {
+    args = args.concat([
+      '--arch',
+      'ia32'
+    ]);
+    spawnPrebuild(args);
+  }
+}
+
+if (require.main === module) {
+  main();
+}
