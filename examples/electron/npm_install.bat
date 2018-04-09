8288b96be21fdfad8ac37a08d7b8a6f2ff3c2494
Daniel Imms <daimms@microsoft.com>
2018-04-09 07:37:30 -0700

    Update electron again

--- examples/electron/npm_install.bat
+++ examples/electron/npm_install.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set npm_config_disturl="https://atom.io/download/electron"
-set npm_config_target=1.7.11
+set npm_config_target=1.8.4
 set npm_config_runtime="electron"
 set npm_config_cache=~\.npm-electron
 npm i
