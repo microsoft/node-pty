cafda284366c8cc2d696531822240e4dd6638695
Gurin, Sebastian <sgurin@netsuite.com>
2018-04-10 22:57:20 -0300

    terminal spec better organization

--- src/terminal.test.ts
+++ src/terminal.test.ts
@@ -30,8 +30,8 @@ describe('Terminal', () => {
     });
   });
 
-  describe('terminal write()',  () => {
 
+  describe('write() basics',  () => {
     it('should emit "data"', (done) => {
       const terminal: Terminal = newTerminal();
       let allTheData = '';
@@ -50,78 +50,59 @@ describe('Terminal', () => {
       terminal.write('world');
     });
 
-    describe('write() callback, input consumption and "drain" event', () => {
-    //   // let shortTime;
-    //   // let longTime;
+    it('should let us know if the entire data was flushed successfully to the kernel buffer or was queued in user memory and in the later case when it finish to be consumed', (done) => {
+      const shortString = 'ls\f';
+      const terminal = newTerminal();
+      terminal.write(shortString, (flushed: boolean) => {
+          terminal.destroy();
+          done && done(); // because we are notified several times and we want to call done once
+          done = null;
+      });
+    });
+  });
 
-    //   it('should notify if the was completely consumed', (done) => {
-    //     const shortString = 'echo 1\f';
-    //     terminal = newTerminal();
-    //     let flushedAlready = false;
-    //     // shortTime = Date.now();
-    //     terminal.write(shortString, (flushed: boolean) => {
-    //       if (!flushedAlready && flushed) {
-    //         flushedAlready = true;
-    //         // shortTime = Date.now() - shortTime;
-    //         terminal.destroy();
-    //         done && done();
-    //         done = null;
-    //       }
-    //     });
-    //   });
-
-      let terminalWriteDescribe;
-      function buildLongInput(): string {
-        const count = process.platform === 'win32' ? 8 : 4;
-        let s = terminalWriteDescribe.toString() + '\f';
-        for (let i = 0; i < count; i++) {
-          s += s;
-        }
-        return s;
+  describe('write() data flush and "drain" event', () => {
+    function buildLongInput(): string {
+      const count = process.platform === 'win32' ? 8 : 6;
+      let s = buildLongInput.toString() + '\f';
+      for (let i = 0; i < count; i++) {
+        s += s;
       }
-      let shouldEmitDrain = false;
-      let drainEmitted = false;
-      let terminal;
+      return s;
+    }
+    let shouldEmitDrain = false;
+    let drainEmitted = false;
 
-      it('should provide meanings to know if the entire data was flushed successfully to the kernel buffer or was queued in user memory', terminalWriteDescribe = (done) => {
-        let longString = buildLongInput();
-        terminal = newTerminal();
-        terminal.on('drain', () => {
-          drainEmitted = true;
-        });
-        let flushedAlready = false;
-        // longTime = Date.now();
-        terminal.write(longString, (flushed: boolean) => {
-          if (!flushedAlready && flushed) {
-            flushedAlready = true;
-            // longTime = Date.now() - longTime;
-            // assert.ok(longTime > shortTime, 'inputs considerably bigger should take more time to flush');
-            // terminal.destroy();// dont destroy the terminal not yet because it could still emit some events we are interested on
-            done && done();
-            done = null;
-          }
-          else {
-            shouldEmitDrain = true;
-          }
-        });
-      }).timeout(4000);
-
-      it('should emit "drain" event to know when the kernel buffer is free again', () => {
-        if (process.platform === 'win32') {
-          assert.ok(true, 'winpty doesn\'t support "drain" event');
-        }
-        else if (shouldEmitDrain) {
-          assert.ok(drainEmitted, '"drain" event should be emitted when input to write cannot be flushed entirely');
+    it('should provide meanings to know if the entire data was flushed successfully to the kernel buffer or was queued in user memory', (done) => {
+      let longString = buildLongInput();
+      const terminal = newTerminal();
+      terminal.on('drain', () => {
+        drainEmitted = true;
+      });
+      let flushedAlready = false;
+      terminal.write(longString, (flushed: boolean) => {
+        if (!flushedAlready && flushed) {
+          flushedAlready = true;
+          terminal.destroy();
+          done && done();
+          done = null;
         }
         else {
-          assert.ok(!drainEmitted, '"drain" event shouldn\'t be emitted if write input was flushed entirely');
+          shouldEmitDrain = true;
         }
       });
+    }).timeout(4000);
 
-      it('clean up', () => {
-        terminal.destroy();
-      });
-
+    it('should emit "drain" event to know when the kernel buffer is free again', () => {
+      if (process.platform === 'win32') {
+        assert.ok(true, 'winpty doesn\'t support "drain" event');
+      }
+      else if (shouldEmitDrain) {
+        assert.ok(drainEmitted, '"drain" event should be emitted when input to write cannot be flushed entirely');
+      }
+      else {
+        assert.ok(!drainEmitted, '"drain" event shouldn\'t be emitted if write input was flushed entirely');
+      }
     });
   });
 });
