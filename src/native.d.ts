1a3ce77d0466572d87077babf650ff39b338d8ff
Daniel Imms <tyriar@tyriar.com>
2018-12-21 03:53:48 -0800

    Type Windows native components

--- src/native.d.ts
+++ src/native.d.ts
@@ -3,16 +3,33 @@
  */
 
 interface IConptyNative {
-  startProcess(file: string, cols: number, rows: number, debug: boolean, pipeName: string): IConptyPty;
+  startProcess(file: string, cols: number, rows: number, debug: boolean, pipeName: string): IConptyProcess;
+  connect(ptyId: number, commandLine: string, cwd: string, env: string[], onProcessExitCallback: (exitCode: number) => void);
+  resize(ptyId: number, cols: number, rows: number): void;
+  kill(ptyId: number): void;
 }
 
 interface IWinptyNative {
-  startProcess(file, commandLine, env, cwd, cols, rows, debug): IConptyPty;
+  startProcess(file, commandLine, env, cwd, cols, rows, debug): IWinptyProcess;
+  resize(processHandle: number, cols: number, rows: number): void;
+  kill(pid: number, innerPidHandle: number): void;
+  getProcessList(pid: number): number[];
+  getExitCode(innerPidHandle: number): number;
 }
 
-interface IConptyPty {
+interface IConptyProcess {
   pty: number;
   fd: number;
   conin: string;
   conout: string;
+}
+
+interface IWinptyProcess {
+  pty: number;
+  fd: number;
+  conin: string;
+  conout: string;
+  pid: number;
+  innerPid: number;
+  innerPidHandle: number;
 }
