2c7f64c2511082ca3710e59b0cfa5fc1ab68dafe
Daniel Imms <daimms@microsoft.com>
2019-01-18 11:45:17 -0800

    Move windows-process-tree to install.js
    Optional dev deps aren't supported by npm

--- scripts/install.js
+++ scripts/install.js
@@ -4,15 +4,27 @@ const os = require('os');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-const args = ['rebuild'];
-if (process.env.NODE_PTY_DEBUG) {
-  args.push('--debug');
-}
-const p = spawn(os.platform() === 'win32' ? 'node-gyp.cmd' : 'node-gyp', args, {
+// windows-process-tree is an optional dev dependency which npm does not support
+const npmArgs = ['i', '--no-save', 'windows-process-tree@0.2.3'];
+const npmProcess = spawn(os.platform() === 'win32' ? 'npm.cmd' : 'npm', npmArgs, {
   cwd: path.join(__dirname, '..'),
   stdio: 'inherit'
 });
 
-p.on('exit', function (code) {
-  process.exit(code);
+npmProcess.on('exit', function (code) {
+  if (code) {
+    throw new Error('npm i windows-process-tree failed with code ' + code);
+  }
+  const gypArgs = ['rebuild'];
+  if (process.env.NODE_PTY_DEBUG) {
+    gypArgs.push('--debug');
+  }
+  const gypProcess = spawn(os.platform() === 'win32' ? 'node-gyp.cmd' : 'node-gyp', gypArgs, {
+    cwd: path.join(__dirname, '..'),
+    stdio: 'inherit'
+  });
+
+  gypProcess.on('exit', function (code) {
+    process.exit(code);
+  });
 });
