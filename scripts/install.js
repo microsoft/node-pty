52f390a366a8bc601efd97c99f8a6ae2eab333a0
Daniel Imms <daimms@microsoft.com>
2019-01-18 11:49:51 -0800

    Only install it on Windows

--- scripts/install.js
+++ scripts/install.js
@@ -4,14 +4,7 @@ const os = require('os');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-// windows-process-tree is an optional dev dependency which npm does not support
-const npmArgs = ['i', '--no-save', 'windows-process-tree@0.2.3'];
-const npmProcess = spawn(os.platform() === 'win32' ? 'npm.cmd' : 'npm', npmArgs, {
-  cwd: path.join(__dirname, '..'),
-  stdio: 'inherit'
-});
-
-npmProcess.on('exit', function (code) {
+installWindowsProcessTree().then(() => {
   if (code) {
     throw new Error('npm i windows-process-tree failed with code ' + code);
   }
@@ -28,3 +21,24 @@ npmProcess.on('exit', function (code) {
     process.exit(code);
   });
 });
+
+// windows-process-tree is an optional dev dependency which npm does not support
+function installWindowsProcessTree() {
+  return new Promise(resolve => {
+    if (process.platform !== 'win32') {
+      resolve();
+      return;
+    }
+    const npmArgs = ['i', '--no-save', 'windows-process-tree@0.2.3'];
+    const npmProcess = spawn(os.platform() === 'win32' ? 'npm.cmd' : 'npm', npmArgs, {
+      cwd: path.join(__dirname, '..'),
+      stdio: 'inherit'
+    });
+    npmProcess.on('exit', function (code) {
+      if (code) {
+        throw new Error('windows-process-tree install failed with code ' + code);
+      }
+      resolve();
+    });
+  });
+}
