f91ebd1320e08223c6dd70717fe069d0c58449ef
Daniel Imms <daimms@microsoft.com>
2019-05-07 20:36:35 -0700

    Expose cols and rows APIs
    Fixes #289

--- src/terminal.ts
+++ src/terminal.ts
@@ -35,6 +35,8 @@ export abstract class Terminal implements ITerminal {
   public get onExit(): IEvent<IExitEvent> { return this._onExit.event; }
 
   public get pid(): number { return this._pid; }
+  public get cols(): number { return this._cols; }
+  public get rows(): number { return this._rows; }
 
   constructor(opt?: IPtyForkOptions) {
     // for 'close'
