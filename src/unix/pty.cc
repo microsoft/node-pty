microsoft/node-pty

commit 5db10ca4e3970a65492ee0bb7c1f02766d7d6628

chore: remove support for really old node versions

src/unix/pty.cc
@@ -34,15 +34,7 @@
 #if defined(__GLIBC__) || defined(__CYGWIN__)
 #include <pty.h>
 #elif defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__)
-/**
- * From node v0.10.28 (at least?) there is also a "util.h" in node/src, which
- * would confuse the compiler when looking for "util.h".
- */
-#if NODE_VERSION_AT_LEAST(0, 10, 28)
-#include <../include/util.h>
-#else
 #include <util.h>
-#endif
 #elif defined(__FreeBSD__)
 #include <libutil.h>
 #elif defined(__sun)
@@ -120,11 +112,7 @@ static void
 pty_waitpid(void *);
 
 static void
-#if NODE_VERSION_AT_LEAST(0, 11, 0)
 pty_after_waitpid(uv_async_t *);
-#else
-pty_after_waitpid(uv_async_t *, int);
-#endif
 
 static void
 pty_after_close(uv_handle_t *);
@@ -476,11 +464,7 @@ pty_waitpid(void *data) {
  */
 
 static void
-#if NODE_VERSION_AT_LEAST(0, 11, 0)
 pty_after_waitpid(uv_async_t *async) {
-#else
-pty_after_waitpid(uv_async_t *async, int unhelpful) {
-#endif
   Nan::HandleScope scope;
   pty_baton *baton = static_cast<pty_baton*>(async->data);
 

commit f7a28cc2287be924d8d184165d4ff25c3ccd25c7

Fix building on OpenBSD, and likely Mac OS X
5db10ca broke compiling on OpenBSD, as
.node-gyp/$NODE_VERSION/src/util.h takes precendence over
/usr/include/util.h.

src/unix/pty.cc
@@ -34,7 +34,11 @@
 #if defined(__GLIBC__) || defined(__CYGWIN__)
 #include <pty.h>
 #elif defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__)
-#include <util.h>
+/**
+ * From node v0.10.28 (at least?) there is also a "util.h" in node/src, which
+ * would confuse the compiler when looking for "util.h".
+ */
+#include <../include/util.h>
 #elif defined(__FreeBSD__)
 #include <libutil.h>
 #elif defined(__sun)