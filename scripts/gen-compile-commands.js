75ac469ca385ca7238c17e6ed49b8fca8c2ed6c4
Robo <hop2deep@gmail.com>
2025-09-23 20:58:44 +0900

    chore: lock node-gyp used for compile commands (#802)

--- scripts/gen-compile-commands.js
+++ scripts/gen-compile-commands.js
@@ -2,5 +2,7 @@
  * Copyright (c) 2025, Microsoft Corporation (MIT License).
  */
 
+const { execSync } = require('child_process');
+
 console.log(`\x1b[32m> Generating compile_commands.json...\x1b[0m`);
-execSync('npx node-gyp configure -- -f compile_commands_json');
+execSync('npx --offline node-gyp configure -- -f compile_commands_json');
