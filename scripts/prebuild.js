603f97508eeabbd7596ef1fec06cf97e1dc3efe3
Devraj Mehta <devm33@github.com>
2025-10-16 22:26:39 -0400

    feat: remove prebuilds when npm_config_build_from_source is set

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -6,19 +6,20 @@ const path = require('path');
 /**
  * This script copies the prebuilt binaries from the prebuild directory
  * for the current platform and architecture to the build/Release directory.
- * 
+ *
  * Usage:
  *     node scripts/prebuild.js
  */
 
-// Skip copying prebuilds when npm_config_build_from_source is set
-if (process.env.npm_config_build_from_source === 'true') {
-  console.log('\x1b[33m> Skipping prebuild copy because npm_config_build_from_source is set\x1b[0m');
-  process.exit(1);
-}
-
 const PREBUILD_DIR = path.join(__dirname, '..', 'prebuilds', `${process.platform}-${process.arch}`);
 const RELEASE_DIR = path.join(__dirname, '../build/Release');
+
+// Skip copying prebuilds when npm_config_build_from_source is set
+if (process.env.npm_config_build_from_source === 'true') {
+  console.log('\x1b[33m> Removing prebuilds and rebuilding because npm_config_build_from_source is set\x1b[0m');
+  fs.rmSync(PREBUILD_DIR, { recursive: true });
+  process.exit(1);
+}
 
 /* Copy prebuild files to build/Release */
 console.log('\x1b[32m> Copying prebuilds to release folder...\x1b[0m');
