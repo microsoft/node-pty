0fe4edab329e7097bf48b9bbce2c7d7628e05213
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2025-12-10 12:42:34 -0800

    fix: delete all prebuilds if not using any (#829)

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -14,21 +14,22 @@ const path = require('path');
  *     node scripts/prebuild.js
  */
 
+const PREBUILDS_ROOT = path.join(__dirname, '..', 'prebuilds');
 const PREBUILD_DIR = path.join(__dirname, '..', 'prebuilds', `${process.platform}-${process.arch}`);
 
-// Skip copying prebuilds when npm_config_build_from_source is set
+// Do not use prebuilds when npm_config_build_from_source is set
 if (process.env.npm_config_build_from_source === 'true') {
   console.log('\x1b[33m> Removing prebuilds and rebuilding because npm_config_build_from_source is set\x1b[0m');
-  fs.rmSync(PREBUILD_DIR, { recursive: true });
+  fs.rmSync(PREBUILDS_ROOT, { recursive: true, force: true });
   process.exit(1);
 }
 
-/* Copy prebuild files to build/Release */
-console.log('\x1b[32m> Copying prebuilds to release folder...\x1b[0m');
+// Check whether the correct prebuilt files exist
+console.log('\x1b[32m> Checking prebuilds...\x1b[0m');
 if (!fs.existsSync(PREBUILD_DIR)) {
-  console.log(`  SKIPPED Prebuild directory ${PREBUILD_DIR} does not exist`);
-  // Exit with 1 to fall back on node-gyp building the native modules
+  console.log(`\x1b[33m> Removing prebuilds and rebuilding because directory ${PREBUILD_DIR} does not exist\x1b[0m`);
+  fs.rmSync(PREBUILDS_ROOT, { recursive: true, force: true });
   process.exit(1);
 }
-// Exit with 0 to skip node-gyp building from source
+
 process.exit(0);
